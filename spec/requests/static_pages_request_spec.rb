2e4ceec60884ea7ba002feafdee67e8951689629
Yasuaki <yasu5293koh@gmail.com>
2020-11-29 06:09:56 +0000

    Add Home,Help,Apout Page、RSpec test controllers and views

---
+++ spec/requests/static_pages_request_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper.rb'
+
+RSpec.describe "StaticPages", type: :request do
+  describe 'Homeページ' do
+    context "Homeページが正しく表示される" do
+      before do
+        get static_pages_home_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("Home | Ruby on Rails Tutorial Sample App")
+      end
+    end
+  end
+  
+  describe 'Helpページ' do
+    context "helpページが正しく表示される" do
+      before do
+        get static_pages_help_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("Help | Ruby on Rails Tutorial Sample App")
+      end
+    end
+  end
+  
+  describe 'Aboutページ' do
+    context "aboutページが正しく表示される" do
+      before do
+        get static_pages_about_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+      it 'タイトルが正しく表示されていること' do
+        expect(response.body).to include("About | Ruby on Rails Tutorial Sample App")
+      end
+    end
+  end
+end
